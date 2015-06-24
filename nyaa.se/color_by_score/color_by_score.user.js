f24a7f70048e7e3b0bf872e83e414d84d1989c7b
Matthieu Michon <matthieu.michon@gmail.com>
2015-06-25 00:22:15 +0200

    Update color_by_score.user.js

--- nyaa.se/color_by_score/color_by_score.user.js
+++ nyaa.se/color_by_score/color_by_score.user.js
@@ -1,16 +1,53 @@
+// ==UserScript==
+// @name         Airliners.net Topic Filter
+// @namespace    https://github.com/MatthieuMichon/UserScripts/
+// @copyright    2015+, Matthieu Michon
+// @license      GNU GPLv3
+// @version      1
+// @description  Colors entries depending on their popularity
+// @author       Matthieu Michon
+// @match        http://www.nyaa.se
+// @grant        none
+// ==/UserScript==
+
+
+/**
+ * Parameters
+ */
+
 var torListQuery = "#main > div.content > table.tlist > tbody > tr.tlistrow";
 var torQueries = ["td.tlistsn", "td.tlistln", "td.tlistdn", "td.tlistmn"];
 var cntCoef = [4, 3, 1, 10];
 
 
+
+/**
+ * getCntFromTor - Get counts (seeders, leechers, dls and msgs) for the given
+ * entry.
+ *
+ * tor: torrent entry
+ *
+ * Returns an array of counters
+ */
+
 function getCntFromTor(tor){
   "use strict";
   var retArray = torQueries.map(function(query){
+    /* apply each query in the torQueries array to the given torrent entry */
     var txt = tor.querySelector(query).textContent;
     return parseInt(txt);
   });
   return retArray;
 }
+
+
+/**
+ * getTorCntArray - Get an array of the four conters per entry
+ *
+ * query: query matching all the entries
+ *
+ * Retruns a 2D array of entries / counters
+ */
 
 function getTorCntArray(query){
   "use strict";
@@ -22,6 +59,15 @@ function getTorCntArray(query){
   return torCntArray;
 }
 
+
+/**
+ * getTorScoreArray - Return an array of scores from an array of entry counters
+ *
+ * array: 2D array (entry / counters)
+ *
+ * Returns an array of scores
+ */
+
 function getTorScoreArray(array){
   "use strict";
   var torScoreArray = array.map(function(cntArray){
@@ -32,6 +78,15 @@ function getTorScoreArray(array){
   return torScoreArray;
 }
 
+
+/**
+ * max - Return the highest value in an array
+ *
+ * array: array of numbers
+ *
+ * Returns the max
+ */
+
 function max(array){
   "use strict";
   return array.reduce(function (prev, curr){
@@ -39,6 +94,16 @@ function max(array){
   });
 }
 
+
+/**
+ * normalize - Normalize the given array against the given max value
+ *
+ * array: array of numbers to normalize
+ * maxValue: target max value
+ *
+ * Returns an array linearly normalized (max = maxValue)
+ */
+ 
 function normalize(array, maxValue){
   "use strict";
   var maxScore = max(array);
@@ -48,6 +113,14 @@ function normalize(array, maxValue){
   return normArray;
 }
 
+/**
+ * getColorArray - Get CSS color from integer
+ *
+ * array: array of integers (range 0 to 255)
+ *
+ * Returns an array of CSS colors "rgb(42, 42, 42)"
+ */
+
 function getColorArray(array){
   "use strict";
   var colorArray = array.map(function(value){
@@ -56,14 +129,26 @@ function getColorArray(array){
   return colorArray;
 }
 
+
+/**
+ * applyColorArray - Apply CSS backgroundColor properties
+ *
+ * torArray: array of entries
+ * colorArray: array of CSS colors "rgb(42, 42, 42)"
+ */
+
 function applyColorArray(torArray, colorArray){
   "use strict";
   //debugger;
   torArray.map(function(tor, index){
     tor.style.backgroundColor = colorArray[index];
-    return;
   });
 }
+
+
+/**
+ * main - Main entry point
+ */
 
 function main(){
   "use strict";
